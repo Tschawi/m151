5f07e33b4cb8e4f70a9f60f45765b44cc8d13d41
Tschawi <89629023+Tschawi@users.noreply.github.com>
2023-02-08 14:20:45 +0100

    Uebung2.2f

--- Uebung2.2/index.php
+++ Uebung2.2/index.php
@@ -1,16 +1,24 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
-$database = "database";
+$username = "root";
+$password = "";
+$database = "northwind";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
 
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully <br> <br>";
+}
+catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage() . "<br>";
 }
 
+//Datenausgabe
+echo "<br>Datenausgabe<br><br>";
+$statement = $conn->prepare("SELECT * FROM customers");
+$statement->execute();
+while($row = $statement->fetch()) {
+    echo $row['first_name']." ".$row['last_name']."<br />";
+}
 ?>
